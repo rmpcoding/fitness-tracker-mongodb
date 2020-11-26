be551f709d3686e0a5bd213bf1de33924a7df98a
Robert Parsons <rmpcoding@gmail.com>
2020-11-25 23:18:23 -0800

    Adds path.resolve to display index.html from backend

--- Router/html-routes.js
+++ Router/html-routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path')
 
 const app = express();
 
 router.get('/', (req, res) => {
-    res.send('hello')
+    res.sendFile(path.resolve('./public/assets/html/index.html'))
 })
 
 module.exports = router;
