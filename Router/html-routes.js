cbb454347d4a43e62857f740b99ef8adf7251206
Robert Parsons <rmpcoding@gmail.com>
2020-11-28 00:35:17 -0800

    Adds express route to login page

--- Router/html-routes.js
+++ Router/html-routes.js
@@ -8,4 +8,8 @@ router.get('/', (req, res) => {
     res.sendFile(path.resolve('./public/assets/html/index.html'))
 })
 
+router.get('/login', (req, res) => {
+    res.sendFile(path.resolve('./public/assets/html/login.html'))
+})
+
 module.exports = router;
