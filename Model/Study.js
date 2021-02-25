87d09c42f7dedab5548c8aab52d9af991a68c93c
Robert Parsons <rmpcoding@gmail.com>
2021-02-24 20:43:28 -0800

    Adds study attributes to study schema (for array/object methods)

--- Model/Study.js
+++ Model/Study.js
@@ -6,27 +6,59 @@ const validator = require('validator').default;
 
 /* -------------- SKELETON OF STUDY SCHEMA - WILL CHANGE SOON -------------- */
 
+/* ------------------------ UPDATE: ARRAY STUDY ADDED ----------------------- */
+
+/* --------------------- STILL NEED DSA STUDY ATTRIBUTES -------------------- */
+
 const studySchema = new mongoose.Schema({
     subject: {
         type: String,
+        trim: true,
+        required: true,
+    },
+    priority: {
+        type: String,
+        trim: true,
     },
     source: {
-        type: mongoose.SchemaTypes.Url,
+        urls: [{type: mongoose.SchemaTypes.Url}],
+    },
+    definition: {
+        type: String,
+        trim: true,
+    },
+    notes: {
+        type: String,
+        trim: true,
+    },
+    understanding: {
+        type: String,
+        trim: true,
+    },
+    method: {
+        type: String,
+        trim: true,
+    },
+    return_value: {
+        type: String,
+        trim: true,
+    },
+    number_of_arguments: {
+        type: Number,
+    },
+    callback_as_first_argument: {
+        type: Boolean,
     },
     time_allotted: {
-        type: String,
+        type: Number,
         default: 25,
     },
     time_spent: {
-        type: String,
+        type: Number,
     },
     date: {
         type: Date,
         default: Date.now,
-    },
-    notes: {
-        type: String,
-        required: true,
     },
     owner: {
         // reference to the authenticated owner
--- pseudocode.js
+++ pseudocode.js
@@ -8,8 +8,14 @@
 // Login
 // User Dashboard
     // Layout
+    
 
 
 /* ------------------------------- Scalability ------------------------------ */
+// Deployment through AWS will allow up to a certain amount of data (512mb) free;
+// However, scale through testing on using faker
+
 
 /* ------------------------------- Deployment ------------------------------- */
+// Connect to AWS by way of MongoATLAS
+// Deploy on Heroku
