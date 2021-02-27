8767c5eaeed661c57a82fe67ecc08fca8a427362
Robert Parsons <rmpcoding@gmail.com>
2021-02-26 18:43:25 -0800

    Adds connection between backend server and frontend html

--- Router/html-routes.js
+++ Router/html-routes.js
@@ -1,15 +1,20 @@
+const e = require('express');
 const express = require('express');
 const router = express.Router();
 const path = require('path')
 
 const app = express();
 
-router.get('/', (req, res) => {
-    res.sendFile(path.resolve('./public/assets/html/index.html'))
+router.get('/signup', (req, res) => {
+    res.sendFile(path.resolve('./public/assets/html/signup.html'))
 })
 
 router.get('/login', (req, res) => {
     res.sendFile(path.resolve('./public/assets/html/login.html'))
+})
+
+router.get('/home', (req, res) => {
+    res.sendFile(path.resolve('./public/assets/html/home.html'))
 })
 
 module.exports = router;
